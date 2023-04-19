0f9a63534cf12081117a176d08be4a2dc010d1db
Anderson Kaiti <124718788+andersonkaiti@users.noreply.github.com>
2023-04-19 16:25:58 -0300

    Alterações

--- app/src/main/java/com/example/recycle/Referencias.java
+++ app/src/main/java/com/example/recycle/Referencias.java
@@ -4,10 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class Referencias extends AppCompatActivity {
+
+    private Button btnLogo, btnImagens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,29 @@ public class Referencias extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        btnLogo = findViewById(R.id.btnLogo);
+        btnImagens = findViewById(R.id.btnImagens);
+
+        btnLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://br.freepik.com/vetores-gratis/sinal-de-reciclagem-verde-ao-redor-da-terra_37408976.htm#query=logo%20reciclagem%20sem%20fundo&;position=16&;from_view=search&;track=ais";
+                Intent linkLogo = new Intent(Intent.ACTION_VIEW);
+                linkLogo.setData(Uri.parse(url));
+                startActivity(linkLogo);
+            }
+        });
+
+        btnImagens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://storyset.com/people";
+                Intent linkImagens = new Intent(Intent.ACTION_VIEW);
+                linkImagens.setData(Uri.parse(url));
+                startActivity(linkImagens);
+            }
+        });
     }
 
     @Override
